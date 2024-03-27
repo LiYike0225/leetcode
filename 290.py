82eec2a6ea2193ef9eb0c0c4e3d06b7922b2204f
LiYike0225 <121000454+LiYike0225@users.noreply.github.com>
2024-03-28 01:07:26 +0800

    Update 290.py

--- 290.py
+++ 290.py
@@ -1,4 +1,4 @@
-lass Solution:
+class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         res=s.split()
         return list(map(pattern.index, pattern)) == list(map(res.index, res))
